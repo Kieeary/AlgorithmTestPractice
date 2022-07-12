3bbfd5ad3066caf15fcf31195cb18cf2da70f5c6
KiBeomChung <kibeom5118@naver.com>
2022-07-13 00:51:41 +0900

    강의 들은 후 코드 수정 (foreach, substring, indexOf 사용)

--- AlgorithmTestPrac/src/StringTestPrac/FindLongestWord.java
+++ AlgorithmTestPrac/src/StringTestPrac/FindLongestWord.java
@@ -7,62 +7,31 @@ public class FindLongestWord {
 	
 	public String Solution(String str) {
 		
-		String str1 = "";
-		String str2 = "";
-				
 		String ans = "";
+		int min = Integer.MIN_VALUE;
+		int length = 0;
 		
-		char [] arr = str.toCharArray();
-		
-		System.out.println(arr.length);
-		System.out.println(str.length());
-		
-		int index = 0;
-		
-		while((arr[index] >= 65 && arr[index] <= 90) || (arr[index] >= 97 && arr[index] <= 122 )) {
-			str1 = str1 + arr[index];
-			index++;
-			if(index >= str.length())	break;
-		} // 첫번째 단어 저장
-		
-		if(index < str.length()) {
-		
-		index++; // 공백에 해당하는 인덱스는 무시
-		
-		while((arr[index] >= 65 && arr[index] <= 90) || (arr[index] >= 97 && arr[index] <= 122 )) {
-			str2 = str2 + arr[index];
-			index++;
-			if(index == str.length())	break;
-		} //두번째 단어 저장
-				
-		if(str1.length() > str2.length()) {
-			ans = str1;
-		} else ans = str2; //  두 단어중 긴 단어가 ans에 저장되어있음
-				
-		String str3 = "";
-		
-		index++;
-		
-		while(index < str.length()) {
-		while((arr[index] >= 65 && arr[index] <= 90) || (arr[index] >= 97 && arr[index] <= 122 )) {
-				str3 = str3 + arr[index];
-				
-				index++;
-				if(index >= str.length())	break;
+		while((length = str.indexOf(" ")) != -1) {
+			if(length + 1 > min) {
+				min = length;
+				ans = str.substring(0, length+1);
+			}
+			str = str.substring(length+1);
 		}
+		if(str.length() > min)	ans = str;
 		
-		if(str3.length() > ans.length()) {
-			ans = str3;
-			str3 = "";
-		} else {
-			str3 = "";
-		}
+		//foreach문을 활용한 풀이	
+//		int min = Integer.MIN_VALUE;
+//		
+//		String [] arr = str.split(" "); // 띄어쓰기를 기준으로 문자열 배열을 저장함
+//		
+//		for(String x : arr) {
+//			if(x.length() > min) {
+//				ans = x;
+//				min = x.length();
+//			}
+//		}
 		
-		if(index == str.length())	break;
-		index++;
-		}
-		} else ans = str1;
-				
 		return ans;
 	}
 
