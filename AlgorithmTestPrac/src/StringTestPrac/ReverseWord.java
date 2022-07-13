233a7eadfc6bf18e530c1207674cbeb3bce5a4d3
KiBeomChung <kibeom5118@naver.com>
2022-07-14 00:08:06 +0900

    강의 듣고 코드 수정 (StrigBuilder이용, 짝지어서 바꾸기 )

--- AlgorithmTestPrac/src/StringTestPrac/ReverseWord.java
+++ AlgorithmTestPrac/src/StringTestPrac/ReverseWord.java
@@ -1,43 +1,54 @@
 package StringTestPrac;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReverseWord {
 	
 	static Scanner sc = new Scanner(System.in);
 	
-	public void Solution(int num) {
-		String[] str = new String[num];
+	public ArrayList<String> Solution(int num, String[] arr) {
+//		ArrayList<String> ans = new ArrayList<>();
+//		
+//		for(String x : arr) {
+//			String tmp = new StringBuilder(x).reverse().toString();
+//			
+//			ans.add(tmp);
+//		}
 		
-		for(int i=0; i<num; i++) {
-			System.out.print("문자열을 입력하세요: ");
-			str[i] = sc.next();
-		}
-		
-		String[] ans = new String[num];
-		
-		for(int i=0; i<num; i++) {
-			int length = str[i].length();
-			ans[i] = "";
+		ArrayList<String> ans = new ArrayList<>();
+		for(String x : arr) {
+			char [] c = x.toCharArray();
+			int lt = 0;
+			int rt = x.length() - 1;
 			
-			for(int n=0; n<length; n++) {
-				ans[i] = ans[i] + str[i].charAt(length-1-n); //마지막 문자부터 추출
+			while(lt<rt) {
+				char tmp = c[lt];
+				c[lt] = c[rt];
+				c[rt] = tmp;
+				lt++;
+				rt--;
 			}
+			String tmp = String.valueOf(c);
+			ans.add(tmp);
 		}
-		
-		for(int i=0; i<num; i++) {
-			System.out.println(ans[i]);
-		}
+		return ans;
 	}
 
 	public static void main(String[] args) {
 		
-		ReverseWord main = new ReverseWord();
-		System.out.print("정수를 입력하세요: ");
-		int num = sc.nextInt();
-	
-		main.Solution(num);
+		ReverseWord main = new ReverseWord();		
 		
+		int num = sc.nextInt();
+		
+		String[] str = new String[num];
+		for(int i=0; i<num; i++) {
+			str[i] = sc.next();
+		}
+		
+		for(String x : main.Solution(num, str)) {
+			System.out.println(x);
+		}
 	}
 
 }
