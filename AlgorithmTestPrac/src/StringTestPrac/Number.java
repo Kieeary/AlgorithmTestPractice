495058ffc881ae9c44f5dc54e3a5f73152ed3a54
KiBeomChung <kibeom5118@naver.com>
2022-08-04 21:47:50 +0900

    1-9 강의 듣고 수정코드(1) - 아스키 코드 이용 + 정수를 뒤에 붙이

--- AlgorithmTestPrac/src/StringTestPrac/Number.java
+++ AlgorithmTestPrac/src/StringTestPrac/Number.java
@@ -3,22 +3,26 @@ package StringTestPrac;
 import java.util.Scanner;
 
 public class Number {
-	
+
 	public int Solution(String str) {
-		
-		String num = str.replaceAll("[^0-9]", "");
-		int newNum = Integer.parseInt(num);
-	
-		return newNum;
+
+		int num = 0;
+
+		for (char c : str.toCharArray()) {
+			if (c >= 48 && c <= 57) {
+				num = num * 10 + (c - 48);
+			}
+		}
+		return num;
 	}
 
 	public static void main(String[] args) {
 
 		Number main = new Number();
 		Scanner sc = new Scanner(System.in);
-		
+
 		String str = sc.next();
-		
+
 		System.out.println(main.Solution(str));
 	}
 
