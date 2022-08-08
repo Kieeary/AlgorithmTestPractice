8f9c4c3f9cec0c4c38272a6add267509a20ad6c4
KiBeomChung <kibeom5118@naver.com>
2022-08-08 21:27:27 +0900

    1-12 강의 듣고 코드 수정 (substring, parseInt, 형변환 이용)

--- AlgorithmTestPrac/src/StringTestPrac/SecretCode.java
+++ AlgorithmTestPrac/src/StringTestPrac/SecretCode.java
@@ -7,34 +7,16 @@ public class SecretCode {
 	
 	public void Solution(String str, int num) {
 		
-		int res = 1;
+		String answer = "";
+		
+		for(int i = 0; i < num; i++) {
+			String s = str.substring(0,7).replace("#", "1").replace("*", "0");
 
-		int result = 0;
-
-		int k = 0;
-
-		for (int i = 1; i <= num; i++) {
-			for (int j = 0 + (7 * (i - 1)); j < 7 * i; j++) {
-				if (str.charAt(j) == '#') {
-					for (int s = 0; s < 6 - (j % 7); s++) {
-						res = res * 2;
-
-//						System.out.println("s: " + s);
-//						System.out.println(res);
-					}
-
-				} else {
-					continue;
-				}
-				result = result + res;
-				res = 1;
-//				System.out.println("j: " + j);
-//				System.out.println("result: " + result);
-			}
-			System.out.printf("%c", result);
-//			System.out.println("=================================");
-			result = 0;
+			answer = answer + (char)Integer.parseInt(s, 2);
+			
+			str = str.substring(7);
 		}
+		System.out.print(answer);
 	}
 
 	public static void main(String[] args) {
