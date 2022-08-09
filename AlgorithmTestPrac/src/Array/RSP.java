8bd5692aab1749767b04f19a893fc9afa5114629
KiBeomChung <kibeom5118@naver.com>
2022-08-09 20:14:06 +0900

    2-3 강의 듣고 수정한 코드 (A가 이길경우만 먼저 생각)

--- AlgorithmTestPrac/src/Array/RSP.java
+++ AlgorithmTestPrac/src/Array/RSP.java
@@ -3,45 +3,28 @@ package Array;
 import java.util.Scanner;
 
 public class RSP {
-	
+
 	static Scanner sc = new Scanner(System.in);
 
-	
-	public char[] Solution(int num, int[] A, int[] B) {
+	public String Solution(int num, int[] A, int[] B) {
+		String answer = "";
 
-		char[] answer = new char[num];
-		
-		for(int i = 0; i<num; i++) {
-			if(A[i] == B[i]) {
-				answer[i] = 'D';
-			}
-			else if(A[i] == 1) {
-				if(B[i] == 2) {
-					answer[i] = 'B';
-				}
-				if(B[i] == 3) {
-					answer[i] = 'A';
-				}
-			}
-			else if(A[i] == 2) {
-				if(B[i] == 1) {
-					answer[i] = 'A';
-				}
-				if(B[i] == 3) {
-					answer[i] = 'B';
-				}
-			}
-			else if(A[i] == 3) {
-				if(B[i] == 1) {
-					answer[i] = 'B';
-				}
-				if(B[i] == 2) {
-					answer[i] = 'A';
-				}
-			}
+		for (int i = 0; i < num; i++) {
+			if (A[i] == B[i])
+				answer += "D";
+
+			else if (A[i] == 1 && B[i] == 3)
+				answer += "A";
+
+			else if (A[i] == 2 && B[i] == 1)
+				answer += "A";
+
+			else if (A[i] == 3 && B[i] == 2)
+				answer += "A";
+
+			else
+				answer += "B";
 		}
-		
-		System.out.print(answer); 
 		return answer;
 	}
 
@@ -49,22 +32,22 @@ public class RSP {
 
 		RSP main = new RSP();
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("입력: ");
 		int num = sc.nextInt();
-		
+
 		int[] A = new int[num];
 		int[] B = new int[num];
-		
-		for(int i = 0; i<num; i++) {
+
+		for (int i = 0; i < num; i++) {
 			A[i] = sc.nextInt();
 		}
-		
-		for(int i = 0; i<num; i++) {
+
+		for (int i = 0; i < num; i++) {
 			B[i] = sc.nextInt();
 		}
-		
-		for(char c : main.Solution(num, A, B)) {
+
+		for (char c : main.Solution(num, A, B).toCharArray()) {
 			System.out.println(c);
 		}
 	}
