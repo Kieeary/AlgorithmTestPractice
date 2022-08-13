1473bdc18b107ccd1907b26043303fc7c7a6d176
Kieeary <kibeom5118@naver.com>
2022-08-13 21:45:47 +0900

    2-11 강의 듣고 코드 수정 (1명의 학생을 집중적으로 비교)

--- AlgorithmTestPrac/src/Array/Election.java
+++ AlgorithmTestPrac/src/Array/Election.java
@@ -3,58 +3,49 @@ package Array;
 import java.util.Scanner;
 
 public class Election {
-	
+
 	public int Solution(int num, int[][] arr) {
-		
+
 		int result[][] = new int[num][num];
-		
+
 		int max = Integer.MIN_VALUE;
 		int answer = 0;
-		
-		for(int i = 0; i<num; i++) {
-			for(int j=0; j<5; j++) {
-				for(int k=0; k <num; k++) {
-					if(arr[i][j] == arr[k][j]) {
-						result[i][k] = 1;
+
+		for (int i = 1; i < num + 1; i++) {
+			int cnt = 0;
+			for (int j = 1; j < num + 1; j++) {
+				for (int k = 1; k < 6; k++) {
+					if (arr[i][k] == arr[j][k]) {
+						cnt++;
+						break;
 					}
 				}
-				
 			}
+			if(max < cnt)	{
+				max = cnt;
+				answer = i;
+			}
+	
 		}
-		
-		for(int i =0; i<num; i++) {
-			int tmp = 0;
-			for(int j=0; j<num; j++) {
-				if(result[j][i] == 1)	tmp++;
-			
-			}
-			
-			if(tmp > max)	{
-				max = tmp;
-				answer = i+1;
-			}
-		}
-		
 		return answer;
-		
 	}
 
 	public static void main(String[] args) {
-		
+
 		Election main = new Election();
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("입력: ");
 		int num = sc.nextInt();
-		int[][] arr = new int [num][5];
-		
-		for(int i =0; i<num; i++) {
-			for(int j =0; j<num; j++) {
-			arr[i][j] = sc.nextInt();
+		int[][] arr = new int[num + 1][6];
+
+		for (int i = 1; i < num + 1; i++) {
+			for (int j = 1; j < 6; j++) {
+				arr[i][j] = sc.nextInt();
 			}
 		}
-		
+
 		System.out.print(main.Solution(num, arr));
 	}
 
