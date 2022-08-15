2e99159fd89b4c42d055f2c478580de8cce8be1a
Kieeary <kibeom5118@naver.com>
2022-08-15 23:30:46 +0900

    3-2 강의 듣고 수정한 코드

--- AlgorithmTestPrac/src/TwoPointersNSlidingWindow/Atomic.java
+++ AlgorithmTestPrac/src/TwoPointersNSlidingWindow/Atomic.java
@@ -1,6 +1,7 @@
 package TwoPointersNSlidingWindow;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Scanner;
@@ -9,24 +10,27 @@ public class Atomic {
 
 	public ArrayList<Integer> Solution(int num1, int num2, int[] arr1, int[] arr2) {
 
-		HashSet<Integer> answer = new HashSet<Integer>();
-		HashSet<Integer> hs2 = new HashSet<Integer>();
+		int p1 = 0;
+		int p2 = 0;
 		
-		for(int i=0; i<num1; i++) {
-			answer.add(arr1[i]);
+		ArrayList<Integer> answer = new ArrayList<Integer>();
+		
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		
+		while(p1 < num1 && p2 < num2) {
+			if(arr1[p1] == arr2[p2]) {
+				answer.add(arr1[p1]);
+				p1++;
+				p2++;
+			}
+			else if(arr1[p1] > arr2[p2]) {
+				p2++;
+			}
+			else p1++;
 		}
 		
-		for(int i=0; i<num2; i++) {
-			hs2.add(arr2[i]);
-		}
-		
-		answer.retainAll(hs2);
-		ArrayList<Integer> result = new ArrayList<Integer>(answer);
-		
-		Collections.sort(result);
-
-		
-		return result;
+		return answer;
 	}
 
 	public static void main(String[] args) {
