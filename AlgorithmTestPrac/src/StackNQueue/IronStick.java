3b9b72e463bd3fe5a834f2c15aa012045adaf32c
Kieeary <kibeom5118@naver.com>
2022-08-24 23:20:28 +0900

    5-5 이전과 원리는 동일. foreach 대신 for문을 이용하여 이전 원소에 더 쉽게 접근하도록 수정

--- AlgorithmTestPrac/src/StackNQueue/IronStick.java
+++ AlgorithmTestPrac/src/StackNQueue/IronStick.java
@@ -8,21 +8,14 @@ public class IronStick {
 	public int Solution(String stick) {
 		
 		int answer = 0;
-		boolean before = false;
 		Stack<Character> stack = new Stack<>();
 		
-		for(char c : stick.toCharArray()) {
-			if(c == '(') {
-				stack.push(c);
-				before = true;
-			} else if(before == false && c == ')'){ // 쇠구슬 
+		for(int i =0 ; i<stick.length(); i++) {
+			if(stick.charAt(i) == '(')	stack.push(stick.charAt(i));
+			else {
 				stack.pop();
-				answer = answer + 1;
-				before = false;
-			} else if(before == true && c == ')') { // 레이저
-				stack.pop();
-				answer = answer + stack.size();
-				before = false;
+				if(stick.charAt(i-1) == '(')	answer = answer + stack.size();
+				else answer = answer + 1;
 			}
 		}
 		
@@ -43,5 +36,3 @@ public class IronStick {
 	}
 
 }
-
-//(((()(()()))(())()))   (()())
