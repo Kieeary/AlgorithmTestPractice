4b3881d21741cc069f906daafece78a37bdcd5c3
Kieeary <kibeom5118@naver.com>
2022-08-25 16:10:52 +0900

    5-6 (큐를 하나만 만듦. 큐의 사이즈가 1 이 될때까지 계속 순회하기 위해서 k의 배수가 아닌경우엔 dequeue를 한 후에 다시 enqueue 를 해서 계속 순회하도록 코드 수정)

--- AlgorithmTestPrac/src/StackNQueue/PrincessSaving.java
+++ AlgorithmTestPrac/src/StackNQueue/PrincessSaving.java
@@ -10,62 +10,19 @@ public class PrincessSaving {
 	public int Solution(int n, int k) {
 		
 		int answer = 0;
-		int tmp = 0;
 		
-		Queue<Integer> queue1 = new LinkedList<>();
-		Queue<Integer> queue2 = new LinkedList<>();
+		Queue<Integer> queue = new LinkedList<>();
 		
-		for(int i = 1; i<= n; i++) {
-			queue1.offer(i);
+		for(int i = 1; i<=n; i++) {
+			queue.offer(i);
 		}
 		
-		while(queue1.size()!= 1 && queue2.size() != 1) {
-
-//			for(int i = 1; i< queue1.size(); i++) {
-//				if(i % )
-//			}
-			
-			for(Iterator<Integer> iterator = queue1.iterator(); iterator.hasNext();) {
-				int index = iterator.next();
-				tmp++;
-				if(tmp % k == 0) {
-					System.out.println("queue1 에서 삭제한 요소: " + queue1.peek());
-					iterator.remove();
-//					if(queue1.size() == 1)	{
-//						break;
-//					}
-
-				} else {
-					queue2.offer(queue1.peek());
-					iterator.remove();
-				}
-			}
-			
-			if(queue1.size() == 1 && queue2.size() == 0)	break;
-			else queue1.clear();
-			
-			for(Iterator<Integer> iterator = queue2.iterator(); iterator.hasNext();) {
-				int index = iterator.next();
-				tmp++;
-				if(tmp % k == 0) {
-					System.out.println("queue2 에서 삭제한 요소: " + queue2.peek());
-					iterator.remove();
-//					if(queue2.size() == 1)	break;
-				} else {
-					queue1.offer(queue2.peek());
-					iterator.remove();
-				}
-			}
-			
-			if(queue2.size() == 1 && queue1.size() == 0)	break;
-			else queue2.clear();
+		while(!queue.isEmpty()) {
+			for(int i =1; i<k; i++)	queue.offer(queue.poll());
+			queue.poll();
+			if(queue.size() == 1)	answer = queue.poll();
 		}
-		
-		System.out.print("while문 빠져나옴");
-		
-		if(queue1.size() == 1) answer = queue1.peek();
-		else answer = queue2.peek();
-		
+
 		return answer;
 		
 	}
