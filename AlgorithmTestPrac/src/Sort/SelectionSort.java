f663cea3cfca79d07d7b2c70046af3a792f53bf1
Kieeary <kibeom5118@naver.com>
2022-08-26 16:29:24 +0900

    6-1 선택정렬(가장 작은 원소를 가지고 있는 인덱스를 저장하여 오름차순 정렬)

--- AlgorithmTestPrac/src/Sort/SelectionSort.java
+++ AlgorithmTestPrac/src/Sort/SelectionSort.java
@@ -7,13 +7,15 @@ public class SelectionSort {
 	public int[] Solution(int[] arr) {
 
 		for (int i = 0; i < arr.length; i++) {
+			int index = i;
 			for (int j = i + 1; j < arr.length; j++) {
-				if(arr[i] > arr[j]) {
-					int tmp = arr[j];
-					arr[j] = arr[i];
-					arr[i] = tmp;
+				if(arr[index] > arr[j]) {
+					index = j; 
 				}
 			}
+			int tmp = arr[i];
+			arr[i] = arr[index];
+			arr[index] = tmp;
 		}
 		return arr;
 	}
