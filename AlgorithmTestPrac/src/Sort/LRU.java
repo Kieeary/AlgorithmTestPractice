748707db5adff26d70448b1a7453757f17feae53
Kieeary <kibeom5118@naver.com>
2022-08-29 22:18:38 +0900

    6-4 (foreach문으로 맨처음 반복문을 수행. 그외에 크게 다른점 없음)

--- AlgorithmTestPrac/src/Sort/LRU.java
+++ AlgorithmTestPrac/src/Sort/LRU.java
@@ -5,22 +5,33 @@ import java.util.Scanner;
 public class LRU {
 	
 	public int[] Solution(int[] cache, int[] arr) {
-		
-		int tmpIdx = 0;
-		int tmp = 0;
-		
-		for(int i = 0; i < arr.length; i++) {
-			tmpIdx = cache.length-1;
-			tmp = arr[i];
+				
+//		for(int i = 0; i < arr.length; i++) {
+//			int tmpIdx = cache.length-1;
+//			int tmp = arr[i];
+//			for(int j = 0; j<cache.length; j++) {
+//				if(tmp == cache[j])	 {
+//					tmpIdx = j;
+//					break;
+//				}
+//			}
+//			
+//			for(int k = tmpIdx-1; k>=0; k--) {
+//				cache[k+1] = cache[k];
+//			}
+//			cache[0] = tmp;
+//		}
+		for(int i : arr) {
+			int tmpIdx = cache.length-1;
+			int tmp = i;
 			for(int j = 0; j<cache.length; j++) {
-				if(tmp == cache[j])	 {
+				if(i == cache[j]) {
 					tmpIdx = j;
 					break;
 				}
 			}
-			
-			for(int k = tmpIdx-1; k>=0; k--) {
-				cache[k+1] = cache[k];
+			for(int k = tmpIdx; k >= 1; k--) {
+				cache[k] = cache[k-1];
 			}
 			cache[0] = tmp;
 		}
