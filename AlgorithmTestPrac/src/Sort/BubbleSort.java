ab8ab8a8f1469c1715ce5e0b0b492340740a2157
Kieeary <kibeom5118@naver.com>
2022-08-29 20:24:41 +0900

    6-3 (버블 정렬 코드 수정. 효율을 위해 필요없는 인덱스 방문을 하는 for문의 인덱스 반복횟수를 줄임)

--- AlgorithmTestPrac/src/Sort/BubbleSort.java
+++ AlgorithmTestPrac/src/Sort/BubbleSort.java
@@ -7,7 +7,7 @@ public class BubbleSort {
 	public int[] Solution(int n, int[] arr) {
 		
 		for(int i = 0; i < n-1; i++) {
-			for(int j = 0; j<n-1; j++) {
+			for(int j = 0; j<n-i-1; j++) {
 				if(arr[j] > arr[j+1]) {
 					int tmp = arr[j+1];
 					arr[j+1] = arr[j];
