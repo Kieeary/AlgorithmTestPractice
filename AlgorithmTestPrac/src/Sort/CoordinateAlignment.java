b71d3f9426f49167fd39091c8520c931c5c7689c
Kieeary <kibeom5118@naver.com>
2022-08-30 21:42:18 +0900

    6-7 사용자 정의 클래스가 Comparable 인터페이스를 상속받도록 구현. compareTo() 오버라이드 하여 Collections.sort() 통해 오름차순 정렬 구현

--- AlgorithmTestPrac/src/Sort/CoordinateAlignment.java
+++ AlgorithmTestPrac/src/Sort/CoordinateAlignment.java
@@ -1,9 +1,10 @@
 package Sort;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
-class Coordinate{
+class Coordinate implements Comparable<Coordinate>{
 	
 	int x;
 	int y;
@@ -12,50 +13,33 @@ class Coordinate{
 		this.x = x;
 		this.y = y;
 	}
+	
+	@Override
+	public int compareTo(Coordinate c) {
+		if(this.x == c.x)	return this.y-c.y;
+		else return this.x - c.x;
+	}
+	
 }
 
 public class CoordinateAlignment {
-	
-	public Coordinate[] Solution(Coordinate[] al) {
-
-		//삽입정렬
-		for(int i =1; i<al.length; i++) {
-			Coordinate tmp = al[i];
-			int j = 0;
-			for(j = i - 1; j>=0 ; j--) {
-				if(tmp.x < al[j].x) {
-					 al[j+1] = al[j];
-					} 
-				else if (tmp.x == al[j].x) {
-					if(tmp.y < al[j].y) {
-						al[j+1] = al[j];	
-					} else break;
-				}
-				else break;
-				}
-			al[j+1] = tmp;
-		}
-		
-		return al;
-	}
 
 	public static void main(String[] args) {
 
-		CoordinateAlignment main = new CoordinateAlignment();
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("입력: ");
 		int n = sc.nextInt();
-		Coordinate[] al = new Coordinate[n];
+		ArrayList<Coordinate> al = new ArrayList<>();
 		
-		for(int i = 0; i<n; i++) {
-			al[i] = new Coordinate(sc.nextInt(), sc.nextInt()); 
+		for(int i =0; i<n; i++) {
+			al.add(new Coordinate(sc.nextInt(), sc.nextInt()));
 		}
 		
-		for(Coordinate i : main.Solution(al)) {
+		Collections.sort(al);
+		
+		for(Coordinate i : al) {
 			System.out.println(i.x + " " + i.y);
 		}
-		
 	}
-
 }
