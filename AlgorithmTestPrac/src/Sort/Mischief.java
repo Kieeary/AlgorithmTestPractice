2c561f221c4de2adf074d834f625c89c9837635e
Kieeary <kibeom5118@naver.com>
2022-08-30 16:13:57 +0900

    6-6 복사본을 만들어, 원래 배열과 복사본 배열의 원소를 하나씩 비교해가며 원소값이 다른 인덱스 번호를 저장하도록 구현

---
+++ AlgorithmTestPrac/src/Sort/Mischief.java
@@ -0,0 +1,45 @@
+package Sort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Mischief {
+
+	public ArrayList<Integer> Solution(int[] arr) {
+		
+		ArrayList<Integer> answer = new ArrayList<>();
+		int[] copy = new int[arr.length];
+		
+		for(int i =0; i<copy.length; i++) {
+			copy[i] = arr[i];
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i = 0; i<arr.length; i++) {
+			if(arr[i] != copy[i])	answer.add(i+1);
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+
+		Mischief main = new Mischief();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("입력 : ");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i =0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i : main.Solution(arr)) {
+			System.out.print(i + " ");
+		}
+	}
+
+}
