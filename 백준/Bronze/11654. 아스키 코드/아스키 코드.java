cf5a98bd4f30ea3b160002141c81248390aebc6f
Kieeary <70502804+Kieeary@users.noreply.github.com>
2022-10-05 23:31:12 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = str.charAt(0);
+        
+        int answer = c;
+        
+        System.out.print(answer);
+    }
+}
