0b8a6e21718acf91e8ac805930245df8820c960f
Kieeary <70502804+Kieeary@users.noreply.github.com>
2022-10-19 21:06:05 +0900

    [level 0] Title: 평행, Time: 8.14 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/README.md
+++ 프로그래머스/lv0/120875. 평행/README.md
@@ -1,10 +1,10 @@
 # [level 0] 평행 - 120875 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875) 
 
 ### 성능 요약
 
-메모리: 74.2 MB, 시간: 0.05 ms
+메모리: 74.4 MB, 시간: 8.14 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -1,30 +1,35 @@
-import java.util.*;
-
 class Solution {
-    public double solution(int[][] dots) {
+    public int solution(int[][] dots) {
         int answer = 0;
-        ArrayList<Double> list = new ArrayList<>();
-        
-        
-        for(int i = 0; i<4; i++){
-            for(int j = i+1; j<4; j++) {
-                double tmp = ((double)dots[j][1] - (double)dots[i][1]) / 
-                    ((double)dots[j][0] - (double)dots[i][0]);
-//                System.out.println(tmp + " ");
-                list.add(tmp);
+        float[] line = new float[6];
+        int index =0 ;
+        for(int i=0;i<3;i++) {
+            for(int j=i+1; j<4;j++) {
+                float xDegree =(float)(dots[i][0]- dots[j][0]);
+                float yDegree = (float)(dots[i][1]-dots[j][1]);
+//                System.out.println(xDegree + " " + yDegree);
+                
+                float result = yDegree / xDegree;
+                System.out.println("Result: " + result);
+                //0같은게 문제인 것 같은데..
+                if(Double.isNaN(result)){ answer++;};
+                line[index] = result;
+//                System.out.println(line[index]);
+                index++;
             }
         }
         
-                while(!list.isEmpty()){
-            double front = list.remove(0);
-            double back  = list.remove(list.size()-1);
-            if(front==back)
-                return 1;
-        }
-
-        // for(int i=0; i< (list.size()/2); i++){
-        //     if(list.get(i).equals(list.get(list.size()-(1+i)))) return 1;
+        // for(int k = 0; k < line.length; k++){
+        //     for(int m=k+1; m<line.length;m++) {
+        //         if (line[k] == line[m]) answer++;
+        //     }
         // }
+        
+        for(int i = 0; i< line.length/2 +1; i++) {
+            if(line[i] == line[line.length - 1 -i]) answer = 1;
+        }
+//        System.out.println("as:"+answer);
+        
         return answer;
     }
 }
