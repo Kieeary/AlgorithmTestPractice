07b26b387590fa505a1876cf370b9c07b5881977
Kieeary <70502804+Kieeary@users.noreply.github.com>
2022-10-26 13:54:10 +0900

    [level 0] Title: 평행, Time: 0.05 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -1,35 +1,31 @@
+import java.util.*;
+
 class Solution {
-    public int solution(int[][] dots) {
+    public double solution(int[][] dots) {
         int answer = 0;
-        float[] line = new float[6];
-        int index =0 ;
-        for(int i=0;i<3;i++) {
-            for(int j=i+1; j<4;j++) {
-                float xDegree =(float)(dots[i][0]- dots[j][0]);
-                float yDegree = (float)(dots[i][1]-dots[j][1]);
-//                System.out.println(xDegree + " " + yDegree);
-                
-                float result = yDegree / xDegree;
-                System.out.println("Result: " + result);
-                //0같은게 문제인 것 같은데..
-                if(Double.isNaN(result)){ answer++;};
-                line[index] = result;
-//                System.out.println(line[index]);
-                index++;
+        ArrayList<Double> list = new ArrayList<>();
+        
+        for(int i = 0; i<4; i++){
+            for(int j = i+1; j<4; j++) {
+                double tmp = ((double)dots[j][1] - (double)dots[i][1]) / 
+                    ((double)dots[j][0] - (double)dots[i][0]);
+
+                list.add(tmp);
             }
         }
         
-        // for(int k = 0; k < line.length; k++){
-        //     for(int m=k+1; m<line.length;m++) {
-        //         if (line[k] == line[m]) answer++;
-        //     }
-        // }
-        
-        for(int i = 0; i< line.length/2 +1; i++) {
-            if(line[i] == line[line.length - 1 -i]) answer = 1;
+        for(int idx = 0; idx < list.size(); idx++){
+        for(int i = idx+1; i < list.size(); i++) {
+            double a = list.get(idx);
+            double b = list.get(i);
+            
+            // if(list.get(idx).equals(list.get(i)))    {
+            //     answer = 1;
+            // }
+            if(a==b)    answer = 1;
         }
-//        System.out.println("as:"+answer);
-        
+            
+        }
         return answer;
     }
 }
