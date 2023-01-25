94ad0f1b3d5f2183f51da0655c3f64abc0ecd967
Kieeary <70502804+Kieeary@users.noreply.github.com>
2023-01-25 17:23:18 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1228 ms, Memory: 135568 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,4 +1,9 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 class Main {
 
@@ -18,18 +23,22 @@ class Main {
 
 	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		N = sc.nextInt();
-		int M = sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
 		int[][] arr = new int[N + 2][N + 2];
 
 		// 그래프 관계 설정
 		for (int i = 0; i < M; i++) {
-			int u = sc.nextInt();
-			int v = sc.nextInt();
+			st = new StringTokenizer(br.readLine());
+			
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
 
 			arr[u][v] = 1;
 			arr[v][u] = 1;
@@ -49,6 +58,7 @@ class Main {
 			}
 		}
 
-		System.out.println(answer);
+		bw.write(String.valueOf(answer));
+		bw.flush();
 	}
 }
