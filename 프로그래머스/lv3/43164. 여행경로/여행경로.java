11936c038d2467db2f49fb85a4ced5752c302028
Kieeary <70502804+Kieeary@users.noreply.github.com>
2023-03-30 04:05:48 +0900

    [level 3] Title: 여행경로, Time: 8.93 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.java
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.java
@@ -1,6 +1,42 @@
+import java.util.*;
+
 class Solution {
+    
+    static ArrayList<String> al = new ArrayList<>();
+    
+    static public void dfs(String start, int cnt, int[] ch, String result, String[][] tickets) {
+        
+        if(cnt == tickets.length)   {
+            al.add(result);
+        }
+        
+        else {
+            for(int i =0; i<tickets.length; i++) {
+                if(start.equals(tickets[i][0]) && ch[i] == 0) {
+                    ch[i] = 1;
+                    cnt++;
+                    dfs(tickets[i][1], cnt, ch, result + " " + tickets[i][1], tickets);
+                    cnt--;
+                    ch[i] = 0;
+                } 
+            }
+        }
+    }
+    
     public String[] solution(String[][] tickets) {
-        String[] answer = {};
+        
+        // 방문 여부 확인
+        int[] ch = new int[tickets.length];
+        String result = "ICN";
+        int cnt = 0;
+        
+        dfs("ICN", cnt, ch, result, tickets);
+        
+        Collections.sort(al);
+        String[] answer = al.get(0).split(" ");
+        //System.out.println(al.get(0));
+        
+        
         return answer;
     }
 }
